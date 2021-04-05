8377e351dde9fd913376d1b5ca44367e0bddfa35
JoseTemox <temistoclesledezma@gmail.com>
2021-04-05 19:03:10 -0400

    pelis

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+
+
+
+@NgModule({
+  declarations: [NavbarComponent, SlideshowComponent],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    NavbarComponent,
+    SlideshowComponent
+  ]
+})
+export class ComponentsModule { }
