e44ed1008c2135d9bdc3ee3238aaeea8dcede132
JoseTemox <temistoclesledezma@gmail.com>
2021-04-06 21:14:27 -0400

    scroll infinito

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -2,15 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CarteleraResponse } from '../interfaces/cartelera-response';
+import { tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculasService {
+  private  baseUrl: string = 'https://api.themoviedb.org/3';
+  private cartelerapage =1;
 
   constructor(private http: HttpClient) { }
 
+  get params(){
+    return{
+      api_key: 'c7d07fb51cf5b374d5b496f2e3ac9bcd',
+      language: 'es-ES',
+      page: this.cartelerapage.toString()
+
+    }
+  }
+
   getCartelera(): Observable<CarteleraResponse>{
-    return this.http.get<CarteleraResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=c7d07fb51cf5b374d5b496f2e3ac9bcd&language=es-ES&page=1')
+    return this.http.get<CarteleraResponse>(`${this.baseUrl}/movie/now_playing`,{
+      params: this.params
+    }).pipe(
+      tap( () => {
+        this.cartelerapage+=1
+      })
+    );
   }
 }
